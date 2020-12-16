28a5a5149422c3e183d01cd80ff8f694f95f044c
akthrms <akt.hrms@gmail.com>
2020-12-17 06:25:46 +0900

    first commit

---
+++ lib/ex_option.ex
@@ -0,0 +1,18 @@
+defmodule ExOption do
+  @moduledoc """
+  Documentation for `ExOption`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExOption.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_option_test.exs
@@ -0,0 +1,8 @@
+defmodule ExOptionTest do
+  use ExUnit.Case
+  doctest ExOption
+
+  test "greets the world" do
+    assert ExOption.hello() == :world
+  end
+end

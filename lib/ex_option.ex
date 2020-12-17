13929693a7d45ba6ab58dfd16d0fdcf3df548ee8
akthrms <akt.hrms@gmail.com>
2020-12-18 06:52:06 +0900

    update

--- lib/ex_option.ex
+++ lib/ex_option.ex
@@ -3,16 +3,64 @@ defmodule ExOption do
   Documentation for `ExOption`.
   """
 
+  @type option :: {:some, any} | {:none}
+
+  @spec none :: option
   @doc """
-  Hello world.
+  No value.
+  """
+  def none(), do: {:none}
+
+  @spec some(any) :: option
+  @doc """
+  Some value.
+  """
+  def some(value), do: {:some, value}
+
+  @spec some?(option) :: boolean
+  @doc """
+  Returns true if the option is a some value.
 
   ## Examples
 
-      iex> ExOption.hello()
-      :world
-
+      iex> ExOption.some(2) |> ExOption.some?()
+      true
+      iex> ExOption.none() |> ExOption.some?()
+      false
   """
-  def hello do
-    :world
-  end
+  def some?({:some, _}), do: true
+
+  def some?({:none}), do: false
+
+  @spec none?(option) :: boolean
+  @doc """
+  Returns true if the option is a none value.
+
+  ## Examples
+
+      iex> ExOption.some(2) |> ExOption.none?()
+      false
+      iex> ExOption.none() |> ExOption.none?()
+      true
+  """
+  def none?({:none}), do: true
+
+  def none?({:some, _}), do: false
+
+  @spec unwrap(option) :: any
+  @doc """
+  Returns the contained some value.
+  Errors if the value equals none.
+
+  ## Examples
+
+      iex> ExOption.some("air") |> ExOption.unwrap()
+      "air"
+
+      iex> ExOption.none() |> ExOption.unwrap()
+      ** (ArgumentError) the value is none
+  """
+  def unwrap({:some, value}), do: value
+
+  def unwrap({:none}), do: raise(ArgumentError, "the value is none")
 end
--- test/ex_option_test.exs
+++ test/ex_option_test.exs
@@ -1,8 +1,4 @@
 defmodule ExOptionTest do
   use ExUnit.Case
   doctest ExOption
-
-  test "greets the world" do
-    assert ExOption.hello() == :world
-  end
 end

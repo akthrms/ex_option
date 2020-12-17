e927bea8d970725e588c2b5328475c85b2eb3b41
akthrms <akt.hrms@gmail.com>
2020-12-18 07:35:45 +0900

    update

--- lib/ex_option.ex
+++ lib/ex_option.ex
@@ -63,4 +63,47 @@ defmodule ExOption do
   def unwrap({:some, value}), do: value
 
   def unwrap({:none}), do: raise(ArgumentError, "the value is none")
+
+  @spec unwrap_or(option, any) :: any
+  @doc """
+  Returns the contained some value or a provided default.
+
+  ## Examples
+
+      iex> ExOption.some("car") |> ExOption.unwrap_or("bike")
+      "car"
+      iex> ExOption.none() |> ExOption.unwrap_or("bike")
+      "bike"
+  """
+  def unwrap_or({:some, value}, _), do: value
+
+  def unwrap_or({:none}, default), do: default
+
+  @spec map(option, fun) :: option
+  @doc """
+  Maps an option T to option U by applying a function to a contained value.
+
+  ## Examples
+
+      iex> ExOption.some("Hello, World!") |> ExOption.map(&String.length/1)
+      {:some, 13}
+  """
+  def map({:some, value}, fun), do: fun.(value) |> some()
+
+  def map({:none}, _), do: none()
+
+  @spec map_or(option, any, fun) :: any
+  @doc """
+  Applies a function to the contained value (if any), or returns the provided default (if not).
+
+  ## Examples
+
+      iex> ExOption.some("foo") |> ExOption.map_or(42, &String.length/1)
+      3
+      iex> ExOption.none() |> ExOption.map_or(42, &String.length/1)
+      42
+  """
+  def map_or({:some, value}, _, fun), do: fun.(value)
+
+  def map_or({:none}, default, _), do: default
 end
